08d94952b0656827d1b57575e9d164da5c52cd1b
harshitaM29 <harshita.mundhe29@gmail.com>
2023-08-15 18:04:05 +0530

    created a relationship betwween user and expense as one to many

--- Expense-Tracker-NodeJS/app.js
+++ Expense-Tracker-NodeJS/app.js
@@ -4,6 +4,8 @@ const cors =  require('cors');
 const sequelize = require('./utils/database');
 const userRoutes = require('./routes/user');
 const expenseRouter = require('./routes/expense');
+const User = require('./models/users');
+const Expense = require('./models/expense');
 const app = express();
 
 app.use(cors());
@@ -11,6 +13,9 @@ app.use(cors());
 app.use(bodyParser.json({ extended: false }));
 app.use('/user',userRoutes);
 app.use(expenseRouter);
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
 
 sequelize.sync().then(res => {
     app.listen(4000);
